0c4dc33632ad982481c3058b784b7a8383921b52
Michaela Foote <michaelafoote96@gmail.com>
2016-02-18 21:24:18 -0700

    First Commit

---
+++ Calculator.java
@@ -0,0 +1,58 @@
+package cse360assign3;
+
+/** Ordered list which allows for deletion, searching, and size change.
+ * @author Michaela Foote
+ * 		Pin: 312
+ * @version February 18, 2016
+ * 
+ */
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Returns the correct total
+	 * 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Add the parameter to the total variable
+	 *  @param value	value added to total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Subtract the parameter from the total variable 
+	 *  @param value	value subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Multiply the total by the total by the parameter 
+	 *  @param value	value multiplied by the total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Divide the total by the parameter
+	 *  @param value	value to divide the total by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** Return a history of all actions as a String
+	 * 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
